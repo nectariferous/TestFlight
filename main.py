07da55101efee62b5f66d20f50874cfa9c8050f4
Nectariferous <likhonexe@gmail.com>
2024-08-09 11:20:00 +0600

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,7 @@ def top_games():
     return jsonify(games[:5])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
